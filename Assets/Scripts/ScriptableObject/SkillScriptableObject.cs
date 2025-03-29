f52decbfbd99b2c8035d9a79b5908874f09d42e2
poi001 <107660181+poi001@users.noreply.github.com>
2025-03-30 05:50:49 +0900

    Add_SkillSO(Dash)

--- Assets/Scripts/ScriptableObject/SkillScriptableObject.cs
+++ Assets/Scripts/ScriptableObject/SkillScriptableObject.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class SkillScriptableObject : MonoBehaviour
+//fileName : 생성되는 에셋의 이름
+//menuName: 에셋을 생성하는 메뉴의 이름.  "/" 를 넣으면 경로가 추가.
+//order : 메뉴 중에서 몇 번째 위치에 표시될지 정하는 값.값이 클 수록 마지막에 표기.
+[CreateAssetMenu(fileName = "SkillSO", menuName = "NewFlyGame/SkillSO", order = 1)]
+public class SkillScriptableObject : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Stat Info")]
+    public string characterName;
+    public string description;
+    public int maxSpeed;
+    public int startSpeed;
+    public int weight;
+    public int maxHP;
+    public int maxMP;
 }
--- Assets/Scripts/ScriptableObject/StatScriptableObject.cs
+++ Assets/Scripts/ScriptableObject/StatScriptableObject.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 //fileName : 생성되는 에셋의 이름
 //menuName: 에셋을 생성하는 메뉴의 이름.  "/" 를 넣으면 경로가 추가.
 //order : 메뉴 중에서 몇 번째 위치에 표시될지 정하는 값.값이 클 수록 마지막에 표기.
-[CreateAssetMenu(fileName = "StatSO", menuName = "NewFlyGame/StatSO/Defalut", order = 0)]
+[CreateAssetMenu(fileName = "StatSO", menuName = "NewFlyGame/StatSO", order = 0)]
 public class StatScriptableObject : ScriptableObject
 {
     [Header("Stat Info")]
